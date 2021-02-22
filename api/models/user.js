18768419d59fb72bef29d090cee4e526d97ea4c8
Lloyd Theophilus <lloydtheophilus@gmail.com>
2021-02-22 02:26:09 +0000

    comparing user login details and asigining token using JWT

--- api/models/user.js
+++ api/models/user.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    email: {type: String, required: true},
+    email: {type: String,
+         required: true,
+        unique: true,
+        //Validating an email address with match regex
+        match: /([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|"([]!#-[^-~ \t]|(\\[\t -~]))+")@[0-9A-Za-z]([0-9A-Za-z-]{0,61}[0-9A-Za-z])?(\.[0-9A-Za-z]([0-9A-Za-z-]{0,61}[0-9A-Za-z])?)+/
+    },
     password: {type: String, required: true}
 });
 
